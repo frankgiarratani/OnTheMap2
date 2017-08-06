8d222029e1ece4c913b5b487f84bc6538af7be17
Frankie Gee <giarratani@gmail.com>
2017-08-06 17:50:44 +0900

    Now connecting to the udacity API, sending a dictionary with the username and password.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -47,17 +47,46 @@ class LoginViewController: UIViewController {
         print("Login Button Pressed")
         
         
-        print(usernameEntered.text!)
-        print(passwordEntered.text!)
+        //print(usernameEntered.text!)
+        //print(passwordEntered.text!)
+        
+        var postRequest = "{\"udacity\": {\"username\": \""
+        postRequest.append(usernameEntered.text!)
+        postRequest.append("\", \"password\": \"")
+        postRequest.append(passwordEntered.text!)
+        postRequest.append("\"}}")
+        
+        print("POST" + postRequest)
+        
+        let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+        
+        request.httpBody = postRequest.data(using: String.Encoding.utf8)
+        print(request.httpBody!)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let range = Range(5..<data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
+        }
+        task.resume()
     }
     
     
     @IBAction func noAccountButtonPressed(_ sender: Any) {
         print("No Account Button Pressed")
+        debugTextLabel.text = "Sign me up for an account."
     }
     
     @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
         print("Forgot Password Button Pressed")
+        debugTextLabel.text = "Take me to the password reset page."
     }
     
     
